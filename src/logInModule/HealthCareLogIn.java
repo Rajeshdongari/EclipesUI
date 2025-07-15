12c34e63197018fad049a66ecac7e3928efe4519
rajeshdongari <rajeshdongari12@gmail.com>
2025-07-15 17:13:59 +0530

    hello

---
+++ src/logInModule/HealthCareLogIn.java
@@ -0,0 +1,14 @@
+package logInModule;
+
+public class HealthCareLogIn {
+
+	public static void main(String[] args) {
+		System.out.println("username");
+		
+		System.out.println("log in");
+		
+		System.out.println("rajesh");
+
+	}
+
+}
